f595ec9fd771557921afa010a548179f7b0fe623
dhruv50ae <linkdp2012@gmail.com>
2022-07-18 17:26:11 +0100

    Use open to write texts

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-# with open("myFile.txt") as file:
-#     contents = file.read()
-#     print(contents)
 
-with open("newFile.txt", mode="w") as file:
-    file.write("This is a new file")
+PLACEHOLDER = "[name]"
+
+
+with open("myFile.txt") as namesFile:
+    names = namesFile.readlines()
+    print(names)
+
+with open("newFile.txt") as letterFile:
+    letterContents = letterFile.read()
+    for name in names:
+        strippedName = name.strip()
+        newLetter = letterContents.replace(PLACEHOLDER, strippedName)
+        print(newLetter)
+        with open(f"./send/letterFor{strippedName}.txt", mode="w") as completedLetter:
+            completedLetter.write(newLetter)
